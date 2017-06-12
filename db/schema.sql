b02e5e020b40646240001afc0a43030eadb59684
Ageel <ashatry@gmail.com>
2017-06-12 01:24:33 -0500

    uploading sql and connection code. errors in sql

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE "burgers_db";
+
+USE "burgers_db";
+
+
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR (255) NOT NULL,
+	devoured BOOLEAN,
+    date_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
